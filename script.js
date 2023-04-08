a74b6659ba41fe930c3e6b644e96e750e9992c0a
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-08 21:26:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
+// class Animal {
+//   constructor(species) {
+//     this._species = species;
+//   }
+
+//   get species() {
+//     return this._species;
+//   }
+
+//   makeSound() {
+//     console.log("The " + this.species + " makes a sound");
+//   }
+// }
+
+// class Cat extends Animal {
+//   purr() {
+//     console.log("purr");
+//   }
+// }
+
+// class Dog extends Animal {
+//   bark() {
+//     console.log("woof");
+//   }
+// }
+// let animal = new Animal("Horse");
+// animal.makeSound();
+// let cat = new Cat("Cat");
+// cat.purr();
+// let dog = new Dog("Dog");
+// dog.bark();
+
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+constructor(species) {
+this._species = species;
+}
 
-  get species() {
-    return this._species;
-  }
+get species() {
+return this._species;
+}
 
-  makeSound() {
-    console.log("The " + this.species + " makes a sound");
-  }
+makeSound() {
+console.log("The " + this.species + " makes a sound");
+}
 }
 
 class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
+purr() {
+console.log("purr");
+}
 }
 
 class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
+bark() {
+console.log("woof");
 }
-let animal = new Animal("Horse");
-animal.makeSound();
-let cat = new Cat("Cat");
-cat.purr();
-let dog = new Dog("Dog");
-dog.bark();
-
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
