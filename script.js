a7f84ed6ac7dd18002581911e547d8f3185dd589
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-08 21:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -46,21 +46,34 @@ return this._species;
 }
 
 makeSound() {
-console.log("The " + this.species + " makes a sound");
+console.log(`The ${this.species} makes a sound.`);
 }
 }
 
 class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
 purr() {
 console.log("purr");
 }
 }
 
 class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
 bark() {
 console.log("woof");
 }
 }
+
+let animal = new Animal("Horse");
+animal.makeSound();
+let cat = new Cat("Cat");
+cat.purr();
+let dog = new Dog("Dog");
+dog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
